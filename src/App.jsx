66c2910b33c1a58d2aaa5e3c439ab94097166bb5
Murtaza Pakawala <murtazapakawala@student.unsw.edu.au>
2026-04-08 11:45:10 +1000

    about me updated

--- src/App.jsx
+++ src/App.jsx
@@ -145,8 +145,7 @@ export default function App() {
           <section id='about' className='panel'>
             <h1>About Me</h1>
             <p>
-              Hi, I&apos;m Murtaza, a final-year Computer Science (Honours)
-              undergraduate student at UNSW Sydney. I enjoy learning about
+              Hi, I&apos;m Murtaza, and I recently graduated from UNSW Sydney with a First Class Bachelor of Science (Honours) in Computer Science. I enjoy learning about
               machine learning and maths, and I love building projects.
               Outside of study, I like travelling, reading, and playing
               tennis.
